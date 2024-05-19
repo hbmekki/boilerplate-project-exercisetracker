a4e95b60f93a1870c35df2522ca2f22ff79004b3
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-05-19 11:27:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+require('./db/database.js');
+
+const usersRouter = require("./routes/usersRoute.js");
+const exercisesRouter = require("./routes/exercisesRoute.js");
+const logsRouter = require("./routes/logsRoute.js");
 
 app.use(cors())
 app.use(express.static('public'))
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+//API routes
+app.use("/api/users/:_id/exercises", exercisesRouter);
+app.use("/api/users/:_id/logs", logsRouter);
+app.use("/api/users", usersRouter);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
