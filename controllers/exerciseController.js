59e8bcae004d0615019538c76d8c2a14a8156c1c
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-05-19 11:32:45 -0400

    Update exerciseController.js

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,1 +1,29 @@
+const Exercise = require('../models/exerciseModel.js');
+const User = require('../models/userModel.js');
 
+
+// Display list of all Authors.
+exports.addExercise = (req, res) => {
+  const exercise = new Exercise({
+        description: req.body.description,
+        duration: req.body.duration,
+        date: req.body.date? new Date(req.body.date) : new Date(),
+        userId: req.params._id
+    });
+    exercise.save()
+    .then((savedExercise) => {
+      User.findById(savedExercise.userId)
+        .then(user => {
+            res.json({
+                username: user.username,
+                _id: user._id,
+                description: savedExercise.description,
+                duration: savedExercise.duration,
+                date: savedExercise.date.toDateString()
+            });
+        })
+    })
+    .catch (err => {
+      console.log(err)
+    })
+};
