9f9472f9981d2bc10ff24f7ec7308a65ba1425a6
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-05-19 11:37:39 -0400

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    username: {
+    type: String,
+    required: true,
+    unique: true,
+  }
+});
+
+module.exports = mongoose.model('users', userSchema);
